3b3292577a8ecdcf24d8053a556a80b6925ab41c
thang2k6adu <thang2k6adu@gmail.com>
2025-02-19 08:01:46 +0700

    error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,12 +2,12 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body:', req.body)
-    console.log('req.query:', req.query)
-    console.log('req.params:', req.params)
-    console.log('req.files:', req.files)
-    console.log('req.cookies:', req.cookies)
-    console.log('req.jwtDecoded:', req.jwtDecoded)
+    // console.log('req.body:', req.body)
+    // console.log('req.query:', req.query)
+    // console.log('req.params:', req.params)
+    // console.log('req.files:', req.files)
+    // console.log('req.cookies:', req.cookies)
+    // console.log('req.jwtDecoded:', req.jwtDecoded)
 
     // Điều hướng sang tầng Service
 
@@ -16,9 +16,7 @@ const createNew = async (req, res, next) => {
       .status(StatusCodes.CREATED)
       .json({ message: 'POST from Controller: API create list boards' })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message,
-    })
+    next(error)
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import exithook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1/index'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
@@ -14,6 +15,9 @@ const START_SERVER = () => {
 
   //Use APIs_V1
   app.use('/v1', APIs_V1)
+
+  //Middleware xử lí lỗi tập trung, chỉ được gọi khi có lỗi xảy ra trong app
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
