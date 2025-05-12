ba48b671b7dba61610ad4754ede63b65745c2898
thang2k6adu <thang2k6adu@gmail.com>
2025-05-12 09:48:53 +0700

    mongodb_aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import ApiError from '~/utils/ApiError'
 import { StatusCodes } from 'http-status-codes'
+import { BOARD_TYPE } from '~/utils/constants'
 
 const createBoard = async (req, res, next) => {
   //Việc Validate dữ liệu BẮT BUỘC phải có ở phía Back-end vì đây là điều cuối để lưu trữ dữ liệu vào database
@@ -8,6 +9,7 @@ const createBoard = async (req, res, next) => {
   const correctValidation = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
     description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPE.PUBLIC, BOARD_TYPE.PRIVATE).required(),
   })
 
   try {
